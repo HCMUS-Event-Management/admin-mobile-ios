82e628c59e121d443c6f811976b3cd203441f047
nguyenson-255 <nguyenson10b12@gmail.com>
2023-07-27 22:53:33 +0700

    fix loading search

--- admin-mobile/Screen/MenuTabBar/Menu/Search/View/EventsTableViewController.swift
+++ admin-mobile/Screen/MenuTabBar/Menu/Search/View/EventsTableViewController.swift
@@ -68,17 +68,20 @@ class EventsTableViewController: UITableViewController {
     }
     
 
-    func search(term: String) {
-        switch try! Reachability().connection {
-          case .wifi:
-            VM.search(term: term)
-          case .cellular:
-            VM.search(term: term)
-          case .none:
-            showToast(message: "Network not reachable", font: .systemFont(ofSize: 12))
-          case .unavailable:
-            showToast(message: "Network not reachable", font: .systemFont(ofSize: 12))
-        }
+    func search(apps: [DataReponseSearch]) {
+        
+        self.VM.apps = apps
+        self.tableView.reloadData()
+//        switch try! Reachability().connection {
+//          case .wifi:
+//            VM.search(term: term)
+//          case .cellular:
+//            VM.search(term: term)
+//          case .none:
+//            showToast(message: "Network not reachable", font: .systemFont(ofSize: 12))
+//          case .unavailable:
+//            showToast(message: "Network not reachable", font: .systemFont(ofSize: 12))
+//        }
     }
 }
 
